7daf545733133a86caf532ecee0b525931e35b90
murilo-henrique060 <henrique.henryconde@gmail.com>
2025-08-15 15:18:07 -0300

    questões 4 a e b completas

---
+++ questao04/pilha.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "pilha.h"
+
+typedef struct topo {
+    float valor;
+    struct topo *prox;
+} Topo;
+
+typedef struct pilha {
+    Topo *topo;
+} Pilha;
+
+
+Pilha* pilha_cria(void) {
+    Pilha *p = (Pilha*)malloc(sizeof(Pilha));
+    p->topo = NULL;
+
+    return p;
+}
+
+void pilha_push(Pilha* p, float v) {
+    Topo* t = (Topo*)malloc(sizeof(Topo));
+    t->valor = v;
+    t->prox = p->topo;
+
+    p->topo = t;
+}
+
+float pilha_pop(Pilha* p) {
+    float v = p->topo->valor;
+
+    Topo* t = p->topo;
+
+    p->topo = t->prox;
+    free(t);
+
+    return v;
+}
+
+
+int pilha_vazia(Pilha* p) {
+    return p->topo == NULL;
+}
+
+
+void pilha_libera(Pilha* p) {
+    while (p->topo != NULL) {
+        Topo* t = p->topo;
+        p->topo = p->topo->prox;
+        free(t);
+    }
+    
+    free(p);
+}
